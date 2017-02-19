1f18fde52ef0b375ae822c0c10763808652305fd
abdullah13314@hotmail.com <abdullah13314@hotmail.com>
2017-02-19 15:53:29 -0800

    Categories for Fastlist

--- FastList/AppDelegate.swift
+++ FastList/AppDelegate.swift
@@ -16,14 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     let locationManager = CLLocationManager()
     var currentLocation = ""
+    let category = ["Undefined","Grocery","Home Chores","Work Chores"]
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
-        //Prevent screen lock
-        UIApplication.shared.isIdleTimerDisabled = true
         initializeRegionMonitoring()
         return true
     }
